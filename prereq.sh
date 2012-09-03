a1a65263ab0e048ee6821a2a7bc3d80c4f61b283
Alexander Kluth <derhartmut@niwohlos.org>
2012-09-03 22:33:23 +0200

    prereq.sh
    Expanded
    
    Signed-off-by: Alexander Kluth <derhartmut@niwohlos.org>

--- prereq.sh
+++ prereq.sh
@@ -2,20 +2,52 @@
 
 #-----------------------------EDIT HERE------------------------------#
 NODE=node
+NPM=npm
 #-----------------------------STOP HERE------------------------------#
 
-${NODE} --version 2> /dev/null
+echo " Checking your setup..."; echo ""
+
+${NODE} --version &> /dev/null
 
 if [ $? -ne 0 ]
 then
-    echo "node.js not found! Please install it or adjust the path in prereq.sh";
+    echo " ! node.js not found! Please install it or adjust the path in prereq.sh";
+    exit
+else
+    echo " * node.js found."
 fi
 
+
+${NPM} --version &> /dev/null 
+
+if [ $? -ne 0 ]
+then
+    echo " ! npm not found! You have node.js installed, so you probably have to set"
+    echo "   the npm-path in prereq.sh correctly"
+else
+    echo " * npm found"
+fi
+
+
 echo "require('hashish');" > test.js
-node test.js 2> /dev/null
+node test.js &> /dev/null
 
 # 
 if [ $? -ne 0 ]
 then
-    npm install hashish
+    echo -n " ! hashish not found, installing..."
+
+    npm install hashish &> npm.install
+
+    if [ $? -ne 0 ]
+    then
+        echo "failed!"
+        echo " ! Failed installing npm! Please check npm.install"
+    else
+        rm npm.install
+    fi
+else
+    echo " * hashish found"
 fi
+
+rm test.js &> /dev/null
