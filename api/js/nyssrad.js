09e8a8f6b5fb1924406aa281e133b5780d3ce47d
Alexander Kluth <derhartmut@niwohlos.org>
2012-10-16 16:15:08 +0200

    Some API cleanup

---
+++ api/js/nyssrad.js
@@ -0,0 +1,88 @@
+/******************************************************************************
+ * Copyright (c) 2012 Alexander Kluth <derhartmut@niwohlos.org>               *
+ *                                                                            *
+ * Permission is hereby granted,  free of charge,  to any  person obtaining a *
+ * copy of this software and associated documentation files (the "Software"), *
+ * to deal in the Software without restriction,  including without limitation *
+ * the rights to use,  copy, modify, merge, publish,  distribute, sublicense, *
+ * and/or sell copies  of the  Software,  and to permit  persons to whom  the *
+ * Software is furnished to do so, subject to the following conditions:       *
+ *                                                                            *
+ * The above copyright notice and this permission notice shall be included in *
+ * all copies or substantial portions of the Software.                        *
+ *                                                                            *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR *
+ * IMPLIED, INCLUDING  BUT NOT  LIMITED TO THE WARRANTIES OF MERCHANTABILITY, *
+ * FITNESS FOR A PARTICULAR  PURPOSE AND  NONINFRINGEMENT.  IN NO EVENT SHALL *
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER *
+ * LIABILITY,  WHETHER IN AN ACTION OF CONTRACT,  TORT OR OTHERWISE,  ARISING *
+ * FROM,  OUT OF  OR IN CONNECTION  WITH THE  SOFTWARE  OR THE  USE OR  OTHER *
+ * DEALINGS IN THE SOFTWARE.                                                  *
+ ******************************************************************************/
+
+/**
+ * This is a simple and example API for using nyssrad.
+ * For the future I will provide APIs in many different languages
+ * (for each language an own repo).
+ *
+ * The API let you send a request and store a key as well as getting
+ * a value from a given key or many values stored in a JSON object.
+ *
+ * To avoid having too many dependencies we will use the good old
+ * XMLHttpRequest object instead of f.e. jQuery for AJAX calls.
+ **/
+
+var Nyssrad = function()
+{
+
+}
+
+// Uses for AJAX access (web 2.0 wooo pew pew pew yay yay yay)
+Nyssrad.prototype.ajax = new XMLHttpRequest();
+
+Nyssrad.prototype.url = "";
+
+Nyssrad.prototype.proxy = null;
+
+
+Nyssrad.prototype.setURL = function(url)
+{
+    this.proxy = url + "nyssrad.php";
+};
+
+Nyssrad.prototype.getValue = function(key, callback)
+{
+    this.ajax.open('GET', this.proxy + '?cmd=set&key=' + key, true);
+
+    this.ajax.onreadystatechange = function()
+    {
+        self = this;
+        if (self.readyState == 4) {
+            var result = self.responseText;
+            callback(result);
+        }
+    }
+
+    this.ajax.send(null);
+}
+
+
+Nyssrad.prototype.setValue = function(key, value, callback)
+{
+    //TODO: Check if value is an array
+
+    this.ajax.open('GET', this.proxy + '?cmd=set&key=' + key + '&value=' + value, true);
+
+    this.ajax.onreadystatechange = function()
+    {
+        self = this;
+        if (self.readyState == 4) {
+            var result = self.responseText;
+            callback(result);
+        }
+    }
+
+    this.ajax.send(null);
+}
+
+
---
+++ api/js/proxy.php
@@ -0,0 +1,54 @@
+<?php
+/******************************************************************************
+ * Copyright (c) 2012 Alexander Kluth <derhartmut@niwohlos.org>               *
+ *                                                                            *
+ * Permission is hereby granted,  free of charge,  to any  person obtaining a *
+ * copy of this software and associated documentation files (the "Software"), *
+ * to deal in the Software without restriction,  including without limitation *
+ * the rights to use,  copy, modify, merge, publish,  distribute, sublicense, *
+ * and/or sell copies  of the  Software,  and to permit  persons to whom  the *
+ * Software is furnished to do so, subject to the following conditions:       *
+ *                                                                            *
+ * The above copyright notice and this permission notice shall be included in *
+ * all copies or substantial portions of the Software.                        *
+ *                                                                            *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR *
+ * IMPLIED, INCLUDING  BUT NOT  LIMITED TO THE WARRANTIES OF MERCHANTABILITY, *
+ * FITNESS FOR A PARTICULAR  PURPOSE AND  NONINFRINGEMENT.  IN NO EVENT SHALL *
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER *
+ * LIABILITY,  WHETHER IN AN ACTION OF CONTRACT,  TORT OR OTHERWISE,  ARISING *
+ * FROM,  OUT OF  OR IN CONNECTION  WITH THE  SOFTWARE  OR THE  USE OR  OTHER *
+ * DEALINGS IN THE SOFTWARE.                                                  *
+ ******************************************************************************/
+/**
+ * This is _not_ the PHP API for nyssrad, it is a proxy to deal avoid the
+ * SameOrigin policy when accessing nyssrad via AJAX.
+ **/
+
+header('text/plain');
+
+$url = 'http://localhost:8888/';
+
+if ($_GET['cmd'] == "get") {
+    $url = $url . 'get/' . $_GET['key'];
+
+    $handle = fopen($url, "r");
+
+    if ($handle) {
+        while (!feof($handle)) {
+            $buffer = fgets($handle, 4096);
+            echo $buffer;
+        }
+
+        fclose($handle);
+    }
+} else if ($_GET['cmd'] == "set") {
+    $url = $url . 'set/' . $_GET['key'] . '/' . $_GET['value'];
+    $handle = fopen($url, "r");
+    fclose($handle);
+} else {
+    echo "nyssrad proxy";
+}
+
+
+?>
